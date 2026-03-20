b0a8053abd5b12134d0b8c45af6cb56accfe5881
mamekhady <mamekhadyleye69@gmail.com>
2026-03-20 11:03:26 +0100

    STEP 1 done

--- ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
+++ ricm.web.GIVEN/impl/httpserver/itf/impl/HttpStaticRequest.java
@@ -17,7 +17,63 @@ public class HttpStaticRequest extends HttpRequest {
 	}
 	
 	public void process(HttpResponse resp) throws Exception {
-	// TO COMPLETE
+
+	    String filename = m_ressname;
+
+	
+	    if (filename.equals("/")) {
+	        filename = "/" + DEFAULT_FILE;
+	    }
+
+	 
+	    if (filename.startsWith("/")) {
+	        filename = filename.substring(1);
+	    }
+
+	    // Construire le chemin complet
+	    String fullpath = m_hs.getFolder() + "/" + filename;
+
+	    java.io.File file = new java.io.File(fullpath);
+
+	    // Si fichier existe
+	    if (file.exists() && file.isFile()) {
+
+	        // Start-line OK
+	        resp.setReplyOk();
+
+	        // Headers
+	        resp.setContentType(HttpRequest.getContentType(filename));
+	        resp.setContentLength((int) file.length());
+
+	        // Début du body
+	        java.io.PrintStream out = resp.beginBody();
+
+	        // Lire et envoyer le fichier
+	        java.io.FileInputStream fis = new java.io.FileInputStream(file);
+
+	        byte[] buffer = new byte[1024];
+	        int bytesRead;
+
+	        while ((bytesRead = fis.read(buffer)) != -1) {
+	            out.write(buffer, 0, bytesRead);
+	        }
+
+	        fis.close();
+
+	    } else {
+
+	        // 404
+	        resp.setReplyError(404, "File not found");
+
+	        resp.setContentType("text/plain");
+
+	        String msg = "404 File not found";
+
+	        resp.setContentLength(msg.length());
+
+	        java.io.PrintStream out = resp.beginBody();
+	        out.print(msg);
+	    }
 	}
 
 }
